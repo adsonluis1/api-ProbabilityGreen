5b25f92ec1381d7faaee6e2ba88355806d9b115a
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-08 13:07:41 -0300

    add rota de checkingGamerOver e mudando a rota de delee e getTimeByName

--- routes/brasileiraoRoutes.js
+++ routes/brasileiraoRoutes.js
@@ -3,13 +3,13 @@ const brasileiraoA = require('../controllers/brasileirao')
 const router = express.Router()
 
 router.get('/', brasileiraoA.getTable)
+router.get('/checkingGameOver/:timeCasa/:timeFora', brasileiraoA.checkingGameOver)
 router.get('/proximosJogos', brasileiraoA.getGamesByProximosJogosCampeonato)
-router.get(/[a-z]+/i, brasileiraoA.getTimeByName)
+router.get('/:time', brasileiraoA.getTimeByName)
 router.post('/addTime', brasileiraoA.addTime)
 router.post('/addProximosJogos', brasileiraoA.addGamesInProximosJogos)
 router.patch('/changingStatistics', brasileiraoA.changingTeamStatistics)
 router.patch('/changingProximosJogos', brasileiraoA.changingTeamStatisticsProximosJogos)
-router.patch('/patchTable', brasileiraoA.patchTable)
-router.delete('/deleteProximosJogos', brasileiraoA.removeGamesProximosJogosCampeonatoByTime)
+router.delete('/deleteProximosJogos/:hora/:data', brasileiraoA.removeGamesProximosJogosCampeonatoByTime)
 
 module.exports = router
