1aad524e1f3c717f81f95e414f17192859f155e2
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-09 08:56:31 -0300

    excluindo rota de checkingGameOver

--- routes/brasileiraoRoutes.js
+++ routes/brasileiraoRoutes.js
@@ -3,7 +3,6 @@ const brasileiraoA = require('../controllers/brasileirao')
 const router = express.Router()
 
 router.get('/', brasileiraoA.getTable)
-router.get('/checkingGameOver/:timeCasa/:timeFora', brasileiraoA.checkingGameOver)
 router.get('/proximosJogos', brasileiraoA.getGamesByProximosJogosCampeonato)
 router.get('/:time', brasileiraoA.getTimeByName)
 router.post('/addTime', brasileiraoA.addTime)
