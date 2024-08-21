3f9128bc5e4cb6125b20a8503db1fd4424116f99
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-21 07:17:56 -0300

    router da La Liga

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const brasileiraoARouter = require('./routes/brasileiraoRoutes')
 const premierLeagueRouter = require('./routes/premierLeague')
 const ligueOneRouter = require('./routes/ligueOne')
+const laLigaRouter = require('./routes/laLiga')
 const cors = require('cors')
 const app = express()
 
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use('/brasileiraoA', brasileiraoARouter)
 app.use('/premierLeague', premierLeagueRouter)
 app.use('/ligueOne', ligueOneRouter)
+app.use('/laLiga', laLigaRouter)
 
 app.get('/', async (req,res)=>{
     res.json({message:'ola mundo'})
